8997abd21193369457e5c55d008710ca42577787
teawithsand <teawithsand@gmail.com>
2023-10-21 21:43:44 +0000

    Added some more data to process

--- aObywatel/src/data/data.ts
+++ aObywatel/src/data/data.ts
@@ -1,10 +1,15 @@
-import raw from "./bp.json"
+import officesRaw from "./bp.json"
+import ludnoscRaw from "./ludnosc.json"
 
-export const offices = raw.resultset.row.filter(r => {
+export const offices = officesRaw.resultset.row.filter(r => {
     if (!r.name) return false
     const san = r.name.toLocaleLowerCase()
-    return san.includes("urząd") && (san.includes("gminy") || san.includes("miasta"))
+    return san.includes("starost") || san.includes("urząd") && (san.includes("gmin") || san.includes("miejs"))
 }).map(r => ({
     name: r.name,
     url: r.url,
-}))
+    place: r.place.toLowerCase(),
+    ludnosc: ludnoscRaw.find(l => l.miasto.toLocaleLowerCase() === r.place.toLocaleLowerCase())?.ludnosc ?? undefined,
+})).filter(r => r.ludnosc !== undefined)
+
+console.log({ offices })
