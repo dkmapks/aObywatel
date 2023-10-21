af0907f81397b13b73e128d763dbf9dfd255de54
teawithsand <teawithsand@gmail.com>
2023-10-21 14:45:15 +0000

    Fixed petitions being saved to users dir

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use(makeCrudRouter('/api/user', __dirname + '/data/users'))
-app.use(makeCrudRouter('/api/petitions', __dirname + '/data/users'))
+app.use(makeCrudRouter('/api/petitions', __dirname + '/data/petitions'))
 
 app.listen(port, () => {
 	console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
