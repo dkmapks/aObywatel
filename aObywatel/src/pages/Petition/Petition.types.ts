fa0d443e06075333cc93a67b4b6f059a08c99f57
teawithsand <teawithsand@gmail.com>
2023-10-21 19:23:08 +0000

    New definition of petition and its states

--- aObywatel/src/pages/Petition/Petition.types.ts
+++ aObywatel/src/pages/Petition/Petition.types.ts
@@ -1,32 +1,55 @@
+import { Location } from "../../components/map/Map"
+
 export enum PetitionSymbol {
     PETITION = 'PETITION',
     PETITION_MULTIPLE = 'PETITIONW',
 }
 
 export enum PetitionStatus {
-    ACCEPTED = 'ACCEPTED',
-    REJECTED = 'REJECTED',
-    ACTIVE = 'ACTIVE',
-    INACTIVE = 'INACTIVE',
-    CLOSED = 'CLOSED',
+    SENT = 'SENT',
+    CANCELLED = "CANCELLED",
+    DROPPED = "DROPPED",
 }
 
 export interface Petition {
     id: string,
     title: string,
+    /**
+     * End-user readable content.
+     */
     description: string,
+    /**
+     * Actual law-complying content.
+     */
+    content: string,
+    /**
+     * Response of petition's destination.
+     */
+    response: string,
+    /**
+     * Whos to receive given paper. Usually some kind of office or bureau.
+     */
+    recipient: string,
+
     signedBy: string[],
     creationDate: number,
+    
     status: PetitionStatus,
+
     author: string,
     authorId: string,
+
     tags?: string[],
+
     parliament?: {
         id: string,
         symbol: PetitionSymbol.PETITION | PetitionSymbol.PETITION_MULTIPLE,
     }
     targetSignatures?: number,
+
     location?: string,
+    coordinates?: Location,
+
     endDate?: number,
     considerationResponse?: string,
 }
