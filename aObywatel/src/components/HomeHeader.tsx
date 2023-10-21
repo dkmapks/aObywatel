410b6585a7c9659e9b65d3743e994eaac6a507b2
Władysław Kuczerenko <MrJustice26>
2023-10-21 14:01:39 +0200

    Save current draft of work

---
+++ aObywatel/src/components/HomeHeader.tsx
@@ -0,0 +1,10 @@
+
+function HomeHeader() {
+    return <div className="flex">
+        <p>Arrow back</p>
+        <h2 className="text-3xl font-medium">Petycje</h2>
+    </div>;
+}
+
+export default HomeHeader;
+
