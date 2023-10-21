f41747d3301e3138d53197365d2f8383972ccb1e
teawithsand <teawithsand@gmail.com>
2023-10-21 13:15:16 +0000

    Fixed backend dirs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,8 @@ const port = 9125
 app.use(express.json())
 app.use(cors())
 
-app.use(makeCrudRouter('/api/user', '/workspaces/aObywatel/backend/data/users'))
+app.use(makeCrudRouter('/api/user', __dirname + '/data/users'))
+app.use(makeCrudRouter('/api/petitions', __dirname + '/data/users'))
 
 app.listen(port, () => {
 	console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
