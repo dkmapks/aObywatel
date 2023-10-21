55ac2503eb85881e01311826e6f6d29d07ce0859
Władysław Kuczerenko <MrJustice26>
2023-10-21 15:11:17 +0200

    Implement home page version without data

--- aObywatel/src/pages/Home.tsx
+++ aObywatel/src/pages/Home.tsx
@@ -2,20 +2,24 @@ import FilterButton from "../components/FilterButton";
 import HomeHeader from "../components/HomeHeader";
 import BaseInput from "../components/BaseInput";
 import { useState } from "react";
+import PetitionButton from "../components/PetitionButton";
 
 
 function HomePage() {
     const [searchTerm, setSearchTerm] = useState("");
 
-    return <div className="bg-neutral-20 px-[20px]">
-        <HomeHeader />
-        <div className="flex gap-x-4 pe-[20px]">
-            <FilterButton onClick={() => console.log("Test")} />
-            <BaseInput className="w-full" size="small" onInput={(e) => setSearchTerm(e.target.value)} />
+    return <div className="bg-neutral-10 px-[20px] h-[100vh]">
+        <div className="fixed top-0 left-0 px-[20px] bg-neutral-10 w-full">
+            <HomeHeader />
+            <div className="flex gap-x-4 pe-[20px] mb-10">
+                <FilterButton onClick={() => console.log("Test")} />
+                <BaseInput className="w-full" size="small" onInput={(e) => setSearchTerm(e.target.value)} />
+            </div>
         </div>
+        <div className="h-[152px]"></div>
         <ul>
-            {[1,2,3,4,5].map(i => {
-                return <li key={i}>{i}</li>
+            {[1,2,3,4,5, 6, 7, 8, 9, 10].map(i => {
+                return <PetitionButton className="mb-4 border border-neutral-40" key={i} to={`/petition/123`}>{i}</PetitionButton>
             })}
         </ul>
     </div>;
